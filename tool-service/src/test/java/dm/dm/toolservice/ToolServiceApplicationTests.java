29bd1f3a62269851602e39ab224a984c5c662702
Dm <DmBalaev91@gmail.com>
2024-01-03 21:50:27 +0300

    Добавлен класс Tool и связанные с ним классы

---
+++ tool-service/src/test/java/dm/dm/toolservice/ToolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dm.dm.toolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
